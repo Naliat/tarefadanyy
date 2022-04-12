0ec06bfa2f987072d1ed49135a276842358b6e01
Naliat <tailan.oliveira1@aluno.ce.gov.br>
2022-04-12 16:54:32 -0300

    Primeiro commit

---
+++ api/src/main/java/br/com/projeto/api/controler/Controle.java
@@ -0,0 +1,15 @@
+package br.com.projeto.api.controler;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controle {
+  @GetMapping("")
+
+  public String mensagem() {
+    return "Olá Dany Boy! :)";
+
+  }
+
+}
